03ffec77c95a2489a14c5023dd445bc0dffeee23
ChunMeng0127 <meng9494@gmail.com>
2026-09-12 19:21:01 +0800

    Fix HTML encoding in Contact phone search assertion

--- tests/BillingControl.Tests/ContactUiTests.cs
+++ tests/BillingControl.Tests/ContactUiTests.cs
@@ -170,9 +170,8 @@ public partial class IntegrationTests
         Assert.DoesNotContain("name=\"ProviderWaId\"", detailsWithFirstAddress);
         var formattedPhoneSearch = Uri.EscapeDataString("+60 (12) 345-6789");
         var phoneSearchUrl = $"/Contacts?search={formattedPhoneSearch}";
-        var phoneSearch = await admin.GetStringAsync(phoneSearchUrl);
-        var echoedSearch = Regex.Match(phoneSearch, "id=\"Search\"[^>]*value=\"([^\"]*)\"").Groups[1].Value;
-        Assert.True(phoneSearch.Contains("+60123456789"), $"Formatted phone search failed. url={phoneSearchUrl}; echoed={echoedSearch}; noResults={phoneSearch.Contains("No contacts found")}");
+        var phoneSearch = WebUtility.HtmlDecode(await admin.GetStringAsync(phoneSearchUrl));
+        Assert.Contains("+60123456789", phoneSearch);
 
         var secondAddressResponse = await PostWithToken(admin, $"/Contacts/Details/{contactId}", "/Contacts/AddAddress", new()
         {
